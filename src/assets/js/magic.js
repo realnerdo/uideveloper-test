2b3c80a689b75da0f768ec6858866c2b0f7258a1
Asael Jaimes <realnerdo@gmail.com>
2016-02-12 16:39:47 -0600

    Added functionality to increase and decrease quantity value

--- src/assets/js/magic.js
+++ src/assets/js/magic.js
@@ -3,9 +3,39 @@ var $          = require('jquery'),
 
 $(function(){
 
-    var $body   = $('body'),
-        $widget = $('#widget-template');
+    /**
+     * Cache elements for further use
+     */
+    var $body      = $('body'),
+        $widget    = $('#widget-template');
 
+    /**
+     * Update quantity input
+     *
+     * @param  JQuery input
+     * @param  String action
+     * @return void
+     */
+    function update_qty(input, action){
+
+        var current_val = parseInt(input.val());
+
+        switch (action) {
+            case 'add':
+                if (current_val >= 1) input.val(current_val + 1);
+                break;
+            case 'substract':
+                if(current_val > 1) input.val(current_val - 1);
+                break;
+        }
+    }
+
+    /**
+     * Render the Handlebar template fo the widget
+     *
+     * @param  Json context
+     * @return void
+     */
     function render_widget(context){
         var source   = $widget.html(),
             template = handlebars.compile(source),
@@ -14,6 +44,11 @@ $(function(){
         $body.append(html);
     }
 
+    /**
+     * Ajax call for jsonp callback
+     *
+     * @return void
+     */
     $.ajax({
         url: 'https://selz.com/embed/itemdata?itemurl=http://selz.co/1rvbgi3&callback=callback',
         jsonpCallback: 'callback',
@@ -21,6 +56,22 @@ $(function(){
         success: function(response){
             render_widget(response);
         }
+    });
+
+    /**
+     * Substract 1 when click in minus button
+     */
+    $body.on('click', '.minus', function(){
+        var $input_qty = $(this).next('.input-qty');
+        update_qty($input_qty, 'substract');
+    });
+
+    /**
+     * Add 1 when click in plus button
+     */
+    $body.on('click', '.add', function(){
+        var $input_qty = $(this).prev('.input-qty');
+        update_qty($input_qty, 'add');
     });
 
 });
